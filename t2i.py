477d98b0b74994ec291a3cf4e79587e80dac9880
Filip Ginter <ginter@cs.utu.fi>
2017-12-29 15:36:33 +0200

    improvements to t2i, full recursion works

--- t2i.py
+++ t2i.py
@@ -1,5 +1,57 @@
 import json
 import pickle
+import itertools
+
+
+def depth(data):
+    #helper function, calculates the dimensionality depth
+    if isinstance(data,int):
+        return 0
+    else:
+        return 1+depth(data[0])
+
+def maxima(data,level,maxima_list):
+    maxima_list[level]=max(len(data),maxima_list[level])
+    if isinstance(data[0],list):
+        for d in data:
+            maxima(d,level+1,maxima_list)
+            
+try:
+    import torch
+
+    def fill_torch(data,tensor,indices,tensor_size):
+        if isinstance(data[0],int): #we hit the bottom, fill the data in
+            assert len(tensor_size)==1 #last dimension, filling in the data
+            data_v=torch.LongTensor(data[:tensor_size[0]])
+            target=tensor[tuple(indices)]
+            target[:data_v.size(0)]=data_v
+        else: #recursive call
+            for i,d in itertools.islice(enumerate(data),tensor_size[0]):
+                fill_torch(d,tensor,indices+[i],tensor_size[1:])
+
+    def to_torch_long_tensor(data,max_seq_length=0):
+        """
+        Turns lists of python integers into torch tensor
+
+        `data`: produced by t2i() and is thus a list (of lists, of lists...) of python integers
+        `max_seq_length`: trim to this maximum. If 0, no trimming, if integer all dimensions (except the first one which is examples), will be trimmed to this length. If list of integers, every example's dimensions will be trimmed to those maxima.
+        """
+        maxima_list=[0]*depth(data) 
+        maxima(data,0,maxima_list)
+        #maxima_list is say [20000, 2386, 66] if our data is 20000 x 2386 x 66
+        if max_seq_length!=0:
+            if isinstance(max_seq_length,int):
+                max_seq_length=[max_seq_length]*(len(maxima_list)-1)
+            #now max_seq_length is something like [1000,66] or [55,55] meaning our tensor will be e.g. [20000,55,55]
+            assert len(max_seq_length)==len(maxima_list)-1, "If max_seq_length is given as a list, it should have length of {} for this data, which has dimensionality {}.".format(len(maxima_list)-1,maxima_list)
+            maxima_list=maxima_list[0:1]+list(min(actual,maximum) for actual,maximum in zip(maxima_list[1:],max_seq_length))
+        out=torch.LongTensor(*maxima_list).zero_()
+        fill_torch(data,out,[],list(out.size()))
+        return out
+except:
+    pass #no torch, no need for to_torch_long_tensor
+        
+
 
 class T2I(object):
 
@@ -57,12 +109,18 @@ class T2I(object):
         `inp`: by default, a string is translated into single index, a sequence is translated into a list of outputs of a recursive call to self().
         `string_as_sequence`: if True, treat strings as sequences, effectively producing character level indices. self(["hi","there"]) will produce [2,3] if False, and [[2, 3], [4, 2, 5, 6, 5]] if True
         """
-        if isinstance(inp,str) and (not string_as_sequence or len(inp)==1):
+        if isinstance(inp,str):
             if train:
-                return self.idict.setdefault(inp,len(self.idict))
-            else:
-                return self.idict.get(inp,self.unknown)
-        else:
+                if string_as_sequence:
+                    return list(self.idict.setdefault(c,len(self.idict)) for c in inp)
+                else:
+                    return self.idict.setdefault(inp,len(self.idict))
+            else: #not train
+                if string_as_sequence:
+                    return list(self.idict.get(c,self.unknown) for c in inp)
+                else:
+                    return self.idict.get(inp,self.unknown)
+        else: #a list, must recurse
             return list(self(item,string_as_sequence,train) for item in inp)
 
     def reverse(self,inp):
